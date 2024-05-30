cc8ef22ca0c594cc0dd4d5d8897aa18b6ad37e34
Panayotis Silintziris <psilintziris@protonmail.com>
2024-05-30 06:25:35 +0300

    add command to close terminal with same shortcut that opens

--- nvim/lua/silintzir/plugins/toggleterm.lua
+++ nvim/lua/silintzir/plugins/toggleterm.lua
@@ -6,6 +6,7 @@ return {
 
     local set = vim.keymap.set
     set('n', '<C-\\>', '<cmd>ToggleTerm direction=float<CR>', { desc = 'Open terminal' })
+    set('t', '<C-\\>', '<cmd>ToggleTermToggleAll<CR>', { desc = 'Close terminal' })
     set('t', '<C-x>', '<C-\\><C-N>', { desc = 'Terminal escape' })
   end,
 }
