c93711d2d69677402a98d6d9273a63ef29d67c2d
Panayotis Silintziris <psilintziris@protonmail.com>
2024-07-06 08:14:36 +0300

    install markdown preview

--- nvim/lua/silintzir/plugins/init.lua
+++ nvim/lua/silintzir/plugins/init.lua
@@ -2,4 +2,12 @@ return {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'nvim-lua/plenary.nvim', -- lua functions that many plugins use
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
 }
