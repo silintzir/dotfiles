4a46e0d7cce470427c98925756127664c5376d74
Panayotis Silintziris <psilintziris@gmail.com>
2024-09-09 09:48:17 +0300

    change tsserver to ts_ls

--- nvim/lua/silintzir/plugins/lsp.lua
+++ nvim/lua/silintzir/plugins/lsp.lua
@@ -143,7 +143,7 @@ return {
           },
         },
       },
-      tsserver = {},
+      ts_ls = {},
       svelte = {},
       tailwindcss = {},
     }
@@ -173,7 +173,7 @@ return {
           local server = servers[server_name] or {}
           -- This handles overriding only values explicitly passed
           -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for tsserver)
+          -- certain features of an LSP (for example, turning off formatting for ts_ls)
           server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
           require('lspconfig')[server_name].setup(server)
         end,
